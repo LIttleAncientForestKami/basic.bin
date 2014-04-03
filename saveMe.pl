fc5bb738ee6fecea3f530c860eec2073009fe944
LittleAncientForestKami <Tomasz.Borek@gmail.com>
2014-04-03 17:50:37 +0200

    This Perl script is not my work, license unknown

--- saveMe.pl
+++ saveMe.pl
@@ -1,5 +1,7 @@
 #!/usr/local/bin/perl
-# credits to Chimpunk and http://www.etherealbits.com/2012/06/the-perl-script-that-may-save-your-life/
+# Credits to Chipmunk and http://www.etherealbits.com/2012/06/the-perl-script-that-may-save-your-life/
+# Ask Chipmunk for license for this, this is not my work.
+
 open(DEV, '/dev/sda2') or die "Can't open: $!\n";
 while (read(DEV, $buf, 4096)) {
   print tell(DEV), "\n", $buf, "\n"
