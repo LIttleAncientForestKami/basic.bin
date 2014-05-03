7bb472c24b3fbaeb72711949ead98599343247c0
LittleAncientForestKami <Tomasz.Borek@gmail.com>
2014-05-03 15:10:58 +0200

    Adding hardcoded quote locally done
    It is easier to port a shell than a shell script.
    	    	-- Larry Wall.

---
+++ add-prog-fortune-quote.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+########################################################################
+# add-prog-fortune-quote - adds new quotations and converts file to .dat
+########################################################################
+#  Copyright (C) {2012}  {Tomasz Borek}
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################
+
+FORTUNE_PATH=.
+# /usr/share/games/fortune
+
+TXT=$FORTUNE_PATH/prQ
+DAT=$FORTUNE_PATH/prQ.dat
+
+echo "Assuming fortune located in $FORTUNE_PATH, and file with quotes being prQ"
+# echo "Quote text please?"
+# read $QUOTE
+
+echo "%
+ cytat 
+		 -- autor 
+" >> $TXT
+strfile -c % $TXT $DAT
