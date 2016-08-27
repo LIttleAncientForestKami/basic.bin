1c68d8edd6fd1ca2adbbd9e3c901a6a08c27396f
LIttleAncientForestKami <Tomasz.Borek@gmail.com>
2016-08-27 20:43:30 +0200

    Notifies via DBUS with icons :-D
    It’s ridiculous to live 100 years and only be able to remember 30 million bytes.  You know, less than a compact disc.  The human condition is really becoming more obsolete every minute.
          -- Marvin Minsky

---
+++ ubuntu/notify.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Notifications: https://wiki.archlinux.org/index.php/Desktop_notifications
+# Icons: https://specifications.freedesktop.org/icon-naming-spec/icon-naming-spec-latest.html
+
+notify-send ${1:-'Bez tematu'} ${2:-'Bez treści'} --icon=${3:-image-loading}
