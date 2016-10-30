a3c94f2b204e6a02b29d9cb550ec4925c463d8cd
LIttleAncientForestKami <Tomasz.Borek@gmail.com>
2016-10-30 21:34:54 +0100

    Small CLI script to check net conn speed via CURL -w with config file, thanks Binoy Babu from AskUbuntu!
    The C Programming Language — A language which combines the flexibility of assembly language with the power of assembly language.

---
+++ ubuntu/ySlow.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# LAFK_pl
+# why my net is slow? with curl, thanks to Binoy Babu from AskUbuntu
+
+function debug() {
+    echo checking $1
+    curl -w @netDebug.conf -o /dev/null -s $1
+}
+
+debug $1
+echo compare...
+debug google.com
+
+
