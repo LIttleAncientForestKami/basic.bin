541b2bd7194b6278849304de6bd2a87f288775f5
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2018-09-27 19:58:15 +0200

    Lepsza wersja przeglądu repo tnie ip/maska:port do schowka, ma kolorki
    First, solve the problem. Then, write the code.
        -- John Johnson

--- ubuntu/instaPrzeglądTegoRepoNieTylkoLokalnie.sh
+++ ubuntu/instaPrzeglądTegoRepoNieTylkoLokalnie.sh
@@ -1,5 +1,10 @@
+set -e
 echo Próba postawienia serwera na webricku
 git instaweb -d webrick -p 5885
 echo Port 5885
-echo IP...
-ip -c -h -br addr
+ip -c -4 -h -br addr
+echo W schowku jest IP z portem
+ip -4 -br a | grep -v DOWN | awk '{print $3 ":5885"}' > tnij
+xclip -selection clipboard < tnij
+rm tnij
+echo Stopuj jak skończysz przez git instaweb --stop
