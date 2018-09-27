b2dea8bbdf2ef564a94b9d4b8cde63fe2b76280e
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2018-09-27 20:12:41 +0200

    Ustawia opis repo dla instaweb Sprawdzając .git/description i działając jeśli nie jest już ustawione
    There are two ways of constructing a software design.  One way is to make it so simple that there are obviously no deficiencies. And the other way is to make it so complicated that there are no obvious deficiencies.
        -- C.A.R. Hoare

--- ubuntu/instaPrzeglądTegoRepoNieTylkoLokalnie.sh
+++ ubuntu/instaPrzeglądTegoRepoNieTylkoLokalnie.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 set -e
 echo Próba postawienia serwera na webricku
+grep Unnamed .git/description
+if [[ $? -eq 0 ]]; then
+    read -p "Opis (krótki) repozytorium proszę, bo jest nijaki: " OPIS
+    echo $OPIS > .git/description
+fi
 git instaweb -d webrick -p 5885
 echo Port 5885
 ip -c -4 -h -br addr
