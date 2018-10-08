8782edd14be270423dfdf39d538d7222563b7d86
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2018-10-08 17:43:48 +0200

    Git instaweb + Aktualizacja Javy via update-java-alternatives wymaga instalacji komend via update-alternatives, skrypt pomocny dodany
    Elegance is not a dispensable luxury but a quality that decides between success and failure.
        -- Edsgar W. Dijkstra

--- ubuntu/instaPrzeglądTegoRepoNieTylkoLokalnie.sh
+++ ubuntu/instaPrzeglądTegoRepoNieTylkoLokalnie.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-set -e
+set -x
 echo Próba postawienia serwera na webricku
 grep Unnamed .git/description
 if [[ $? -eq 0 ]]; then
     read -p "Opis (krótki) repozytorium proszę, bo jest nijaki: " OPIS
     echo $OPIS > .git/description
 fi
+set -e
 git instaweb -d webrick -p 5885
 echo Port 5885
 ip -c -4 -h -br addr
--- ubuntu/notify.sh
+++ ubuntu/notify.sh
@@ -8,4 +8,6 @@ export $(egrep -z DBUS_SESSION_BUS_ADDRESS /proc/$(pgrep -u $LOGNAME xfce4-sessi
 # Po jakimś czasie sprawdź czy libnotify poprawili i czy można nadpisywać notyfikacje
 # rozważ zabicie notify-osd kiedyś, by nadpisać notkę
 
-notify-send "${1:-'Bez tematu'}" "${2:-'Bez treści'}" --icon=${3:-image-loading}
+notify-send "${1:-'Bez tematu'}" "${2:-'Bez treści'}" --icon=${3:-gnome-do}
+# image-loading gnome-do computer-fail bash application-default-icon
+# I="/usr/share/icons/gnome/32x32/status/computer-fail.png"
