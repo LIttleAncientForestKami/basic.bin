46e770e13cf3070ef1a2be19a8818f94eadd9e5d
Tomasz "LAFK_pl" Borek <Tomasz.Borek@gmail.com>
2020-04-25 20:27:29 +0200

    Mantra 2.0: solidne opcje z getopt Nazwa, Kod czy nie, JDK11 lub 8, -x Bashowe...
    DUŻA zmiana:
    
    1. getopt do KANONIZACJI opcji
    2. opcje długie i krótkie
    3. pomoc - skrypt ma dokumentację!
    4. parsowanie opcji via CASE
    5. test opcji, by zobaczyć czy parsowanie dobrze idzie
    6. bloki kodu zrobione na funkcje: maven, pom, pomoc, test_opcji, repo
    7. wieloliniowa wiadomość w git-quote, ładniej wygląda
    8. katalog zapisany do schowka, by można było zrobić cd Shift+Ins i
    Enter po skrypcie
    9. uproszczona konfiguracja dzięki opcjom domyślnym
    10. obsługa nieznanej kombinacji opcji
    
    Do tego testy opcji mantry.
    
    Detal: w przypominajce do szkoleń jest informacja, że jest pokaz Adoca
    na GitHubie. Niezwiązane z tą zmianą.
    
    Good code is its own best documentation.
        -- Steve McConnell

--- mantry/mantra.sh
+++ mantry/mantra.sh
@@ -1,21 +1,104 @@
 #!/bin/bash
 set +x
-NAZWA=${1:-"nowyProjekt"}
-JDK=${2:-"11"}
-BAZA=${3:-"pl/lafk/"}
+function pomoc() {
+    echo "Mantra: stawia projekt. JDK, Maven, Git, itp. Autor: Tomasz LAFK_pl Borek"
+    echo """Przyjmuje długie i krótkie opcje, także mieszane.
+Składnia:
 
-echo "Zwykle Kod, tym razem $(pwd)"
+mantra - domyślne ustawienia (JDK11, Maven 3, katalog nowyProjekt w bieżącym katalogu, .git, .gitignore, pierwsza migawka z readme i strukturą projektową).
 
-mkdir -p "$NAZWA"/src/{main,test}/{java,resources}/"$BAZA"
-cd "$NAZWA" || exit
-if [[ $JDK = "1.8" ]]; then
-    curl -L -s https://gist.githubusercontent.com/LIttleAncientForestKami/c9b185c123fc97f6022861f645766aa5/raw/45db276f570fcca357fbcf36b6209517c69c6427/pom.xml > pom.xml
-else curl -L -s https://gist.githubusercontent.com/LIttleAncientForestKami/c9b185c123fc97f6022861f645766aa5/raw/00658d664edeef427c295c15eb353728cbbe8f05/pom.xml > pom.xml
-fi
+mantra -n nazwaProjektuIJegoKatalogu (reszta ustawień - domyślna)
 
-sed -i s/#NAME/"$NAZWA"/g pom.xml
-sed -i s/#APP/"$NAZWA"/g pom.xml
+mantra --nazwa=MyProject --bazacom/firma/tjb --repo --kod postawi projekt w Kodzie, bez repo, podstawowe pakiety to com.firma.tjb, nazwa myProject, reszta domyślnie.
 
+Długie opcje można pisać na którykolwiek z 3 sposobów tu prezentowanych: --nazwaAba --nazwa Aba --nazwa=Aba
+Krótkie opcje można mieszać: -x8k -k8x, kolejność nie ma znaczenia, wyjąwszy:
+
+1. p - pomoc kończy działanie programu.
+2. opcje z parametrami, następny element powinien być parametrem!
+3. x - informacje z tej opcji spływają od momentu jej sparsowania - im szybciej, tym więcej.
+
+Opcje: 
+        -x | --set-x-bash -- drukuje komendy przed ich wykonaniem, diagnostyczna
+        -r | --repo     -- ustawia repozytorium na: brak, GitHub, GitLab (domyślnie), BitBucket
+        -n | --nazwa    -- precyzuje nazwę projektu i katalogu projektowego
+        -b | --baza     -- określa pakiet bazowy, domyślnie pl/lafk
+        -8 | --jdk8     -- jeśli nie 8, to 11 (domyślne)
+        -k | --kod      -- użycie tej opcji umieści projekt w Kodzie, razem z większymi projektami, pominięcie utworzy tam gdzie wywołano skrypt
+        -p | --pomoc    -- wyświetli tę wiadomość i zakończy działanie programu.
+        -t | --test     -- testuje przekaz opcji, diagnostyczna (kończy działanie programu).
+
+RDZ:
+# większe projekty: KOD, reszta jak leci, tmp czy gdziekolwiek
+# git lub nie
+# lab, hub, bb, z odpowiednim 'origin' i zdalnym repo
+# strona mana! :D :D :D
+# kompletowanie składni :D <3 :O
+"""
+}
+
+function test_opcji() {
+    # powtórka dla testu
+    GDZIE="${GDZIE}/${NAZWA}"
+    # test właściwy
+    echo $0 $OPCJE
+    echo Nazwa: $NAZWA JDK: $JDK BAZA: $BAZA GDZIE: $GDZIE REPO: $REPO GIST: $GIST
+    exit 0
+}
+
+OPCJE=`getopt -o xn:8kb:r:pt --long set-x-bash,nazwa:,jdk8,kod,baza:,repo:,pomoc,test -n 'mantra' -- "$@"`
+
+eval set -- "$OPCJE"
+NAZWA="nowyProjekt"
+JDK="11"; GIST="00658d664edeef427c295c15eb353728cbbe8f05"
+BAZA="pl/lafk/"
+REPO=GitLab
+GDZIE=$(pwd)
+
+while true; do
+    case "$1" in
+        -x | --set-x-bash ) set -x; shift;;
+        -r | --repo ) REPO="$2"; shift 2;;
+        -n | --nazwa ) NAZWA="$2"; shift 2;;
+        -b | --baza ) BAZA="$2"; shift 2;;
+        -8 | --jdk8 ) JDK="1.8"; GIST="45db276f570fcca357fbcf36b6209517c69c6427"; shift;;
+        -k | --kod ) GDZIE="/home/tammo/Kod"; shift;;
+        -p | --pomoc ) pomoc; exit 0;;
+        -t | --test ) test_opcji; exit 0;;
+        -- ) shift; break;;
+        * ) echo "mantra: Nani?! Mamy nieobsłużoną kombinację?! ${1} ${2}"; break;;
+    esac
+done
+GDZIE="${GDZIE}/${NAZWA}"
+
+
+function pom() {
+    curl -L -s https://gist.githubusercontent.com/LIttleAncientForestKami/c9b185c123fc97f6022861f645766aa5/raw/${GIST}/pom.xml > pom.xml
+}
+
+function maven() {
+    mkdir -p "$NAZWA"/src/{main,test}/{java,resources}/"$BAZA"
+    cd "$NAZWA" || exit
+    pom
+    sed -i s/#NAME/"$NAZWA"/g pom.xml
+    sed -i s/#APP/"$NAZWA"/g pom.xml
+}
+
+function repo() {
+    curl -L -s https://www.gitignore.io/api/java,maven,linux,intellij,vim >> .gitignore
+    # patrz na: https://github.com/toptal/gitignore.io/issues/360 i 186#issuecomment-249601023, przekomplikowali
+    # dużo tekstu w tym gi a różnica dla intellij+all, intellij+iml, intellij niewielka, zrobienie .idea/ i *.iml lepsze. Jak nie Intellij, to JetBrains (+all/iml itp.)
+    echo ".idea/" >> .gitignore
+    echo "*.iml" >> .gitignore
+    git init
+    git add .
+    # RDZ: wiadomość wieloliniowa może? czyli z pliku?
+    git-quote "Mantra projektu wg LAFK_pl
+JDK $JDK, $REPO, Maven, readme i .gitignore 
+Good POM wg LAFK_pl: https://lafkblogs.wordpress.com/2019/09/29/good-pom/"
+}
+
+maven
 cat << EOF > readme.adoc
 = About $NAZWA
 
@@ -31,10 +114,7 @@ Author: @LAFK_pl
 
 EOF
 
-curl -L -s https://www.gitignore.io/api/java,maven,linux,intellij,vim >> .gitignore
-echo ".idea/" >> .gitignore
-echo "*.iml" >> .gitignore
-git init
-git add .
-git-quote "Mantra projektu dla JDK $JDK + Git, Maven, readme i .gitignore Good POM wg LAFK_pl: https://lafkblogs.wordpress.com/2019/09/29/good-pom/"
-idea pom.xml
+repo
+idea pom.xml &
+pwd | xclip -i
+echo mantra: cd Shift Ins
--- mantry/szkieletSzkolenia/przypominajka.sh
+++ mantry/szkieletSzkolenia/przypominajka.sh
@@ -4,3 +4,4 @@ echo 'Szablon: -T $ADOC_TMPLT_PATH' $1
 echo 'RevealJS po nowemu:  asciidoctor-revealjs -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.6.0 X.adoc'
 echo Obrazki z netu: -a allow-uri-read $1
 echo PDF asiidoctor-pdf [diagram] [read-uri] [szablon] $1
+echo Pokaz Adoca na GH jak coś
